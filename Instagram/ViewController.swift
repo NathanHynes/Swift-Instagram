b4ab67b38a0e8572b9fc4f5824d578008d40bdb1
Nathan Hynes <nathanmail@hotmail.co.uk>
2020-01-29 16:30:01 +0000

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Nathan Hynes on 29/01/2020.
+//  Copyright © 2020 Nathan Hynes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
